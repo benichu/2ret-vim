bbefe45f31928461d6f5b4605d12cc1e033cf88b
Ben Thouret <ben@2ret.com>
2012-09-28 16:45:09 -0400

    boostrap fixes.

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,12 @@ die() {
     exit 1
 }
 
+lnif() {
+    if [ ! -e $2 ] ; then
+        ln -s $1 $2
+    fi
+}
+
 echo "thanks for installing 2ret-vim\n"
 
 # Backup existing .vim stuff
@@ -18,18 +24,26 @@ echo "backing up current vim config\n"
 today=`date +%F-%T`
 for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -e $i ] && [ ! -L $file ] && mv $i $i.$today; done
 
+if [ ! -e $endpath/.git ]; then
+    echo "cloning 2ret-vim\n"
+    git clone --recursive http://github.com/benichu/2ret-vim.git $endpath
+else
+    echo "updating 2ret-vim\n"
+    cd $endpath && git pull
+fi
 
-echo "cloning 2ret-vim\n"
-echo "backing up '2ret-vim' git repository before safely cloning latest version\n"
-mv $endpath $endpath.$today
-git clone --recursive http://github.com/benichu/2ret-vim.git $endpath
-mkdir -p $endpath/.vim/bundle
+echo "setting up symlinks"
 ln -s $endpath/.vimrc $HOME/.vimrc
 ln -s $endpath/.vimrc.bundles $HOME/.vimrc.bundles
 ln -s $endpath/.vim $HOME/.vim
+if [ ! -d $endpath/.vim/bundle ]; then
+    mkdir -p $endpath/.vim/bundle
+fi
 
-echo "Installing Vundle\n"
-git clone http://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+if [ ! -e $HOME/.vim/bundle/vundle ]; then
+    echo "Installing Vundle\n"
+    git clone http://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+fi
 
 echo "installing plugins using Vundle\n"
 vim -u $endpath/.vimrc.bundles - +BundleInstall! +BundleClean +qall
